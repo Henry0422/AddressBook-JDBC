6a17b615d8857060a2bb704897c20dd1c580fdd8
Henry0422 <ronglihan@uofs-10-227-161-152.usask.ca>
2015-03-17 21:43:31 -0600

    Create Project

---
+++ src/addressbook/Driver.java
@@ -0,0 +1,19 @@
+package addressbook;
+
+import java.sql.*;
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Connection conn = DriverManager.getConnection("")
+		try{
+			
+		}
+		catch(Exception exc){
+			exc.printStackTrace();
+		}
+	}
+
+}
